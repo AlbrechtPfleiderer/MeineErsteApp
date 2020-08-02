644ece5c8929e97719fa822ea8b553b0cd75c00d
Albrecht Pfleiderer <Albrecht.Pfleiderer@bsz-fs.de>
2020-08-03 00:58:37 +0200

    Initial commit
    (cherry picked from commit c99a535b2acae79beefdb56148ed08082d97fd46)

--- app/src/main/java/berufsschulefreising/javaalberto/de/MainActivity.java
+++ app/src/main/java/berufsschulefreising/javaalberto/de/MainActivity.java
@@ -2,13 +2,46 @@ package berufsschulefreising.javaalberto.de;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import static android.graphics.Color.RED;
+import static android.graphics.Color.YELLOW;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        float size = new Button(this).getTextSize();
+        Toast.makeText(this,"Textgröße " + size, Toast.LENGTH_LONG).show();
+        title = (TextView)findViewById(R.id.textView_titel1);
+        Button red = (Button)findViewById(R.id.button_rot);
+        Button yellow = (Button)findViewById(R.id.button_gelb);
+        Button end = (Button)findViewById(R.id.button_Ende);
+        red.setOnClickListener(this);
+        yellow.setOnClickListener(this);
+        end.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view)
+    {
+     switch(view.getId())
+     {
+         case R.id.button_rot:
+             title.setBackgroundColor(RED);
+             break;
+         case R.id.button_gelb:
+             title.setBackgroundColor(YELLOW);
+         break;
+         case R.id.button_Ende:
+             finish();
+     }
     }
 }
