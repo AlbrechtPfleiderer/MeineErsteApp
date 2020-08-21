ecb980880542642ffa68112a600cbba8c16ea8ba
Albrecht Pfleiderer <Albrecht.Pfleiderer@bsz-fs.de>
2020-08-21 23:37:59 +0200

    Zweiter commit 21.8.
    (cherry picked from commit c99a535b2acae79beefdb56148ed08082d97fd46)

--- app/src/main/java/berufsschulefreising/javaalberto/de/MainActivity.java
+++ app/src/main/java/berufsschulefreising/javaalberto/de/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
          break;
          case R.id.button_Ende:
              finish();
+             break;
      }
     }
 }
